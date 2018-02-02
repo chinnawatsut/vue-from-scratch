0a19b082fab7ce6a6c688afec2af549d1a24bb60
Chinnawat K <chinnawatsut@gmail.com>
2018-02-02 16:09:06 +0700

    add ref.

--- 04-component/app.js
+++ 04-component/app.js
@@ -6,11 +6,11 @@ var localComponent = {
     template: '<div>Local Component</div>',
     data: function() {
       return {
-          
+
       }
     }
 }
-
+// https://vuejs.org/v2/guide/components.html#What-are-Components
 var app = new Vue({
     el:'#app',
     components: {
