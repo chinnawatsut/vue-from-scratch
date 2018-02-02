244a0e1692f977f066633814b5f9746135b9a658
Chinnawat K <chinnawatsut@gmail.com>
2018-02-02 11:46:56 +0700

    binding

--- app.js
+++ app.js
@@ -4,3 +4,47 @@ var app = new Vue({
         message: 'Hello Vue!'
     }
 })
+
+var app2 = new Vue({
+    el:'#app-2',
+    data:{
+        message: 'This is second app'
+    }
+})
+
+var app3 = new Vue({
+    el: '#app-3',
+    data:{
+        seen: true
+    }
+})
+
+var app4 = new Vue({
+    el: '#app-4',
+    data: {
+        todos: [
+            {text: 'Sony'},
+            {text: 'Canon'},
+            {text: 'Nikon'}
+        ]
+    }
+})
+
+var app5 = new Vue({
+    el: '#app-5',
+    data: {
+        message: 'Hello, Vue!'
+    },
+    methods: {
+        reverseMessage: function() {
+            this.message = this.message.split('').reverse().join('')
+        }
+    }
+})
+
+var app6 = new Vue({
+    el: '#app-6',
+    data: {
+        message: ''
+    }
+})
